46e7a8b50083c54bc74e8e9551f707be9fde1542
eyepax-ErandiS <erandi.s@eyepax.com>
2024-06-19 15:57:34 +0530

    Create the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import "./layout.scss";
+import HomePage from "./routes/homePage/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/ListPage";
+import Layout from "./routes/layout/Layout";
+import Register from "./routes/register/Register";
+import ProfilePage from "./routes/profilePage/ProfilePage";
+import Login from "./routes/login/Login";
+import SinglePage from "./routes/singlePage/SinglePage";
 
 function App() {
-  return <div className="layout">Hello World</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
